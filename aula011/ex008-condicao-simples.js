d1c214827afe7402f7dcb3a291d7f93faac40bee
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-02-16 09:42:52 -0400

    Aula 11

---
+++ aula011/ex008-condicao-simples.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log("Você ultrapassou a velocidade permitida. Multado!")
+}
+console.log("Dirija sempe usando cinto de segurança")
