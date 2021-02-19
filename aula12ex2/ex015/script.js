f602f51f9b0ec0e0d58747243c73c06ebb0e5685
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-02-19 01:27:58 -0400

    exercicio 12 completo

--- aula12ex2/ex015/script.js
+++ aula12ex2/ex015/script.js
@@ -14,14 +14,40 @@ function verificar(){
         var gênero =''
         var img = document.createElement('img')  //tag img criada dinamicamente
         img.setAttribute('id', 'foto')  //colocando dinamicamente um id = foto para a tag img
-        if (fsexo[0].checked){
+        if (fsexo[0].checked){   //verificando se a primeira tag radio esta marcada
             gênero = "Homem"
+            if (idade >=0 && idade < 10){
+                //crianca
+                img.setAttribute('src', "criancamenino.png")  //seta os atributos do img com um novo src e seu valor
+            }else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', "jovem.png")
+            }else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', "homem.png")
+            }else{
+                //idoso
+                img.setAttribute('src', "idoso.png")
+            }
         }else{
             gênero = "Mulher"
+            if (idade >=0 && idade < 10){
+                //crianca
+                img.setAttribute('src', "criancamenina.png")
+            }else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', "garota.png")
+            }else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', "mulher.png")
+            }else{
+                //idoso
+                img.setAttribute('src', "idosa.png")
+            }
         }
-        res.innerHTML = `Deterctamos ${gênero} com ${idade} anos.`
+        res.style.textAlign = "center"
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)    //adiciona abaixo do texto a variavel imagem
     }
-
-
 }
 
