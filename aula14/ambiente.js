7a8e85d33a2608150499d8684187608304dcaed3
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-02-26 21:07:46 -0400

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,24 @@
+console.log("Vai começar...")
+for(var c =1; c <=4 ; c++){
+    console.log(c)
+}
+console.log("FIM!")
+
+var c = 1
+var c2 = 10
+while(c <= 5){
+    console.log(c)
+    c++
+}
+
+
+do{
+    console.log(c)
+    c++
+} while(c <= 15)
+
+
+
+for(c=1;c<=5;c++){
+    console.log(c)
+}
