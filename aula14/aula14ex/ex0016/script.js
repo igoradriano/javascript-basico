0f9a64fdd198809a5f9cfd6d1eabbcffa72055a4
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-02-28 21:38:05 -0400

    loops

--- aula14/aula14ex/ex0016/script.js
+++ aula14/aula14ex/ex0016/script.js
@@ -1,1 +1,21 @@
-alert("Olá!")
+function contar(){
+    let inicio = window.document.getElementById("txti")
+    let fim= window.document.getElementById("txtf")
+    var passo = window.document.getElementById("txtp")
+    let res = window.document.getElementById("res")
+    //condição para não existencia de entrada
+    if(inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){ 
+        window.alert('[ERRO] Faltam dados!')
+    } else{
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)   //Transformando em número
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} `
+        }
+    }
+  
+}
