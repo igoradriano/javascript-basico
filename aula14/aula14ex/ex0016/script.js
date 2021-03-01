0ded89b0bcaa22a3d2d3b500a43caea4a1f2e279
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-02-28 23:19:38 -0400

    Update script.js

--- aula14/aula14ex/ex0016/script.js
+++ aula14/aula14ex/ex0016/script.js
@@ -4,18 +4,29 @@ function contar(){
     var passo = window.document.getElementById("txtp")
     let res = window.document.getElementById("res")
     //condição para não existencia de entrada
-    if(inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){ 
-        window.alert('[ERRO] Faltam dados!')
+    if((inicio.value.length == 0) || (fim.value.length == 0) || (passo.value.length == 0)){ 
+        res.innerHTML = "Impossível contar!"
+        //window.alert('[ERRO] Faltam dados!')
     } else{
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inicio.value)   //Transformando em número
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if (p <= 0){
+            window.alert("Passo inválido! Considerando Passo 1!")
+            p = 1
         }
+
+        if (i < f){
+            //CONTAGEM CRESCENTE
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // CONTAGE REGRESSIVA
+            for(let c = i; c >=f; c-= p)
+                res.innerHTML += `${c} \u{1F449}`
+        }  
+        res.innerHTML += `\u{1F3C1}` 
     }
-  
 }
