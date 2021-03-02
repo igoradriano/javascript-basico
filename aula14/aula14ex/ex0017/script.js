defff501670b7fb457b6ebf8d2e3010e2dc1f365
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-03-01 22:55:03 -0400

    Exercicios 16 e 17

--- aula14/aula14ex/ex0017/script.js
+++ aula14/aula14ex/ex0017/script.js
@@ -1,1 +1,18 @@
-alert("Olá!")
+function tabuada(){
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert("Por favor, digite um número!")
+    }else {
+        let n = Number(num.value)
+        c = 1
+        tab.innerHTML = ''   //limpando o elemento select
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text =`${n} X ${c} = ${n*c}`
+            item.value = `tab${c}` //adicionando o value a cada item
+            tab.appendChild(item) //Adicionando os options ao elemento selec 
+            c++
+        }
+    }
+}
