601204670b5612172e4ecdc3c2dfa463defb98a9
Igor Adriano de Carvalho Rodrigues <igoradrianodcr@gmail.com>
2021-03-05 08:20:50 -0400

    Update script.js

--- aula16/ex0017/script.js
+++ aula16/ex0017/script.js
@@ -4,7 +4,7 @@ let tab = document.getElementById('seltab')
 let lista = []
 
 function adicionar(){
-    res.innerHTML = ``
+    
     if (num.value.length == 0) {
         window.alert("Por favor, digite um número!")
     }else {
@@ -16,6 +16,7 @@ function adicionar(){
                 let item = document.createElement('option')
                 item.text = `Valor ${n} adicionado`
                 tab.appendChild(item)
+                res.innerHTML = `` //apagar o resultado final, pois adicionei mais um elemento
             }else{
                 window.alert("O valor já foi adicionado! Tente outro valor")
             }
@@ -24,22 +25,38 @@ function adicionar(){
         }
             
     }
+    num.value = ``   //para apagar o valor do campo numeros assim que inserir
+    num.focus();   //para colocar o cursos no campo de inserir numeros 
 }
 
 
 function finalizar(){
-    let tam = Number(lista.length)
-    let maior = Math.max(lista)
-    let menor = Math.min(lista)
-    let soma = 0
-    for (i in lista){
-        soma += lista[i]
+    if(lista.length == 0){
+        window.alert("Adicione valores antes de finalizar!")
+    }else{
+        let tam = Number(lista.length)
+        let maior = lista[0]
+        let menor = lista[0]
+        for (let pos in lista){
+            if (lista[pos] > maior){
+                maior = lista[pos]
+            }
+            if (lista[pos] < menor){
+                menor = lista[pos]
+            }
+        }
+        let soma = 0
+        for (i in lista){
+            soma += lista[i]
+        }
+        let media = (soma/tam)
+        res.innerHTML = `<p>Ao todo, temos ${tam} números cadastrados!</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+
     }
-    let media = (soma/tam)
-    res.innerHTML = `<p>Ao todo, temos ${tam} números cadastrados!</p>`
-    res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
-    res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
-    res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
-    res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+    
 
 }
